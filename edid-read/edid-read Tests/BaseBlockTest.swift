44cc08aa44863cbf775bf59d03c02121473d724e
jackielwu <jackielwu@gmail.com>
2023-02-08 23:09:29 -0800

    Add unit test

---
+++ edid-read/edid-read Tests/BaseBlockTest.swift
@@ -0,0 +1,22 @@
+//
+//  BaseBlockTest.swift
+//  edid-read Tests
+//
+//  Created by Jackie Wu on 2/8/23.
+//
+
+@testable import edid_read
+import XCTest
+
+final class BaseBlockTest: XCTestCase {
+    var baseblock: BaseBlock!
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+}
